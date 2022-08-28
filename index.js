0007d2cccd63a9f2f55beacc25e03927f6c8208b
Emerson de Lemmus <edelemmus@gmail.com>
2022-08-27 19:23:11 -0500

    update icon logos

--- index.js
+++ index.js
@@ -44,14 +44,14 @@ const readme = `\
 ## :link: Links
 
 <p align="center">
-  <a href="https://www.linkedin.com/in/edelemmus/"><img src="https://www.vectorlogo.zone/logos/linkedin/linkedin-icon.svg" alt="linkedin"/></a>
-  <a href="mailto:edelemmus@gmail.com"><img src="https://www.vectorlogo.zone/logos/gmail/gmail-icon.svg" alt="email"/></a>
-  <a href="https://open.spotify.com/user/12144018445"><img src="https://www.vectorlogo.zone/logos/spotify/spotify-icon.svg" alt="spotify"/></a>
-  <a href="https://twitter.com/emersondelemmus"><img src="https://www.vectorlogo.zone/logos/twitter/twitter-tile.svg" alt="twitter"/></a>
-  <a href="https://www.facebook.com/emersondelemmus/"><img src="https://www.vectorlogo.zone/logos/facebook/facebook-official.svg" alt="facebook"/></a>
-  <a href="https://www.instagram.com/emersondelemmus/"><img src="https://www.vectorlogo.zone/logos/instagram/instagram-icon.svg" alt="instagram"/></a>
-  <a href="https://medium.com/@edelemmus"><img src="https://img.icons8.com/color/96/000000/medium-logo.png" alt="medium"/></a>
-  <a href="https://stackoverflow.com/users/8859867/emerson-a-de-lemmus-ii"><img src="https://www.vectorlogo.zone/logos/stackoverflow/stackoverflow-tile.svg" alt="stackoverflow"/></a>
+  <a href="https://www.linkedin.com/in/edelemmus/"><img height="80" img src="https://www.vectorlogo.zone/logos/linkedin/linkedin-icon.svg" alt="linkedin"/></a>
+  <a href="mailto:edelemmus@gmail.com"><img height="80" img src="https://www.vectorlogo.zone/logos/gmail/gmail-icon.svg" alt="email"/></a>
+  <a href="https://open.spotify.com/user/12144018445"><img height="80" img src="https://www.vectorlogo.zone/logos/spotify/spotify-icon.svg" alt="spotify"/></a>
+  <a href="https://twitter.com/emersondelemmus"><img height="80" img src="https://www.vectorlogo.zone/logos/twitter/twitter-tile.svg" alt="twitter"/></a>
+  <a href="https://www.facebook.com/emersondelemmus/"><img height="80" img src="https://www.vectorlogo.zone/logos/facebook/facebook-official.svg" alt="facebook"/></a>
+  <a href="https://www.instagram.com/emersondelemmus/"><img height="80" img src="https://www.vectorlogo.zone/logos/instagram/instagram-icon.svg" alt="instagram"/></a>
+  <a href="https://medium.com/@edelemmus"><img height="80" img src="https://img.icons8.com/color/96/000000/medium-logo.png" alt="medium"/></a>
+  <a href="https://stackoverflow.com/users/8859867/emerson-a-de-lemmus-ii"><img height="80" img src="https://www.vectorlogo.zone/logos/stackoverflow/stackoverflow-tile.svg" alt="stackoverflow"/></a>
 </p>
 
 
