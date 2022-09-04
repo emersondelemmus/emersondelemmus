ac4c593bd608e84d531a8908d0ebaf91cfd1d0bd
Emerson de Lemmus <edelemmus@gmail.com>
2022-09-04 17:55:01 -0500

    index

--- index.js
+++ index.js
@@ -195,6 +195,8 @@ const readme = `\
 
 [![emkun's github stats](https://github-readme-stats.vercel.app/api/top-langs/?username=emersondelemmus&hide=html,css)](https://github.com/anuraghazra/github-readme-stats)
 [![Top Langs](https://github-readme-stats.vercel.app/api?username=emersondelemmus&show_icons=true&count_private=true&line_height=40)](https://github.com/anuraghazra/github-readme-stats)\
+[![emkun's github activity](https://activity-graph.herokuapp.com/graph?username=emersondelemmus)](https://github.com/ashutosh00710/github-readme-activity-graph)
+
 `
 
 console.log(readme)
